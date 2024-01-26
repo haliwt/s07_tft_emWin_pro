3692961459f3f3ad2c198b4ca4236d4e392b53dc
haliwt <jonewang@126.com>
2024-01-26 08:24:14 +0800

    numbers of pixel 96x96 data be fill done.

--- Bsp/inc/bsp_tft_font.h
+++ Bsp/inc/bsp_tft_font.h
@@ -3,40 +3,36 @@
 #include "main.h"
 
 //extern const unsigned char asc2_1224[95][36];
-
-
 //extern const uint8_t font1616_temp[];//[][16];
-extern const uint8_t font2424_temp[3][72];
-
 //extern const uint8_t font1616_temp_symbol[];
 
 //extern const uint8_t font1616_humidity_symbol[];
 //extern const uint8_t font3232_humidity_symbol[];
 
-
 //extern const uint8_t font1616_humidity[];
 //extern const uint8_t font3232_humidity[2][128];
 
-extern const uint8_t font2424_humidity[3][72];
-
-
-
 //extern const uint8_t font1616_works_time[];
-extern const uint8_t font2424_works_time[4][72];
-extern const uint8_t font4848_works_time_value[11][144];
-
-
-
-
 
 //extern const uint8_t font4848_no[4][144];
 
 //extern const uint8_t font6464_no[1][256];
 
-extern const uint8_t font9696_no[5][576];
-
-
 //extern const uint8_t ASCII8x16_Table [] ;
+
+
+
+extern const uint8_t font2424_temp[3][72];
+extern const uint8_t font2424_humidity[3][72];
+
+extern const uint8_t font2424_works_time[4][72];
+extern const uint8_t font4848_works_time_value[11][144];
+
+extern const uint8_t font9696_no[10][576];
+
+
+
+
 
 
 
