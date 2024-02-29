030ad0606110aad45a52594ee9f19ae950c58c84
haliwt <jonewang@126.com>
2024-02-29 16:52:59 +0800

    Update auto link tencent cloud is success .

--- Bsp/src/bsp_wifi.c
+++ Bsp/src/bsp_wifi.c
@@ -12,6 +12,10 @@ uint8_t auto_det_flag;
 static void MainBoard_Self_Inspection_PowerOn_Fun(void);
 static void RunWifi_Command_Handler(void);
 static void auto_repeat_link_netware_fun(void);
+static void auto_repeat_init_link_net(void);
+static void auto_repeat_tencnet_net(void);
+
+
 
 
 
@@ -166,6 +170,10 @@ static void RunWifi_Command_Handler(void)
 			           wifi_t.runCommand_order_lable= wifi_get_beijing_time;
 					   wifi_t.set_beijing_time_flag =1;
                 }
+				else{
+
+
+				}
 				
 
 				}
@@ -350,8 +358,10 @@ static void auto_repeat_link_netware_fun(void)
   if(auto_det_flag==1 && wifi_t.power_on_login_tencent_cloud_flag !=5){
         //Buzzer_KeySound();
       
-		InitWifiModule_Hardware();//InitWifiModule();
-        SmartPhone_TryToLink_TencentCloud();
+		//InitWifiModule_Hardware();//InitWifiModule();
+        auto_repeat_init_link_net();
+        //SmartPhone_TryToLink_TencentCloud();
+        auto_repeat_tencnet_net();
   }
 
     if(wifi_link_net_state()==1){
@@ -423,5 +433,35 @@ void Wifi_Rx_Auto_Link_Net_Handler(void)
 }
 
 
+static void auto_repeat_init_link_net(void)
+{
+	    WIFI_IC_DISABLE();
+		HAL_Delay(1000);
+		//HAL_Delay(1000);
+		//HAL_Delay(1000);
+		WIFI_IC_ENABLE();
+		//at_send_data("AT+RESTORE\r\n", strlen("AT+RESTORE\r\n"));
+		at_send_data("AT+RST\r\n", strlen("AT+RST\r\n"));
+		HAL_Delay(1000);
 
+
+
+}
+
+static void auto_repeat_tencnet_net(void)
+{
+            wifi_t.linking_tencent_cloud_doing =1;
+          
+			wifi_t.wifi_uart_counter=0;
+	        wifi_t.soft_ap_config_flag =0;
+	        HAL_UART_Transmit(&huart2, "AT+TCMQTTCONN=1,5000,240,0,1\r\n", strlen("AT+TCMQTTCONN=1,5000,240,0,1\r\n"), 5000);//开始连接
+		    HAL_Delay(1000);
+			HAL_Delay(1000);
+			HAL_Delay(1000);
+		
+		
+		 
+
+
+}
 
