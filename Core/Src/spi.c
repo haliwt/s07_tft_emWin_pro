b04d42ebfe1d69ac2bd5c5e14a0f9ba9aad6d875
haliwt <jonewang@126.com>
2024-03-23 10:41:29 +0800

    spi interrupt of set up number :1

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -88,7 +88,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /* SPI1 interrupt Init */
-    HAL_NVIC_SetPriority(SPI1_IRQn, 3, 0);
+    HAL_NVIC_SetPriority(SPI1_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(SPI1_IRQn);
   /* USER CODE BEGIN SPI1_MspInit 1 */
 
