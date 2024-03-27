879982f961957bc770dcf9026ac2e280ac7711ac
haliwt <jonewang@126.com>
2024-03-27 18:52:38 +0800

    sample number:2  codes.DATA.2023.03.27 --> 18.52

--- Bsp/src/interrupt_manager.c
+++ Bsp/src/interrupt_manager.c
@@ -42,26 +42,26 @@ void USART_Cmd_Error_Handler(void)
 		
      }
 
-	 if(gctl_t.gTimer_ctl_dma1_error > 2){
-
-	   gctl_t.gTimer_ctl_dma1_error=0;
-
-	    __HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_TE);
-		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_NO_XFER);
-		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_TIMEOUT);
-		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_PARAM);
-		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_BUSY);
-		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_NOT_SUPPORTED);
-		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_SYNC);
-		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_REQGEN);
-
-//	  __HAL_SPI_CLEAR_OVRFLAG(&hspi1) ;
-//     __HAL_SPI_CLEAR_CRCERRFLAG(&hspi1);
-//      __HAL_SPI_CLEAR_MODFFLAG(&hspi1);
-//       __HAL_SPI_CLEAR_FREFLAG(&hspi1);
-
-
-	 }
+//	 if(gctl_t.gTimer_ctl_dma1_error > 2){
+//
+//	   gctl_t.gTimer_ctl_dma1_error=0;
+//
+//	    __HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_TE);
+//		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_NO_XFER);
+//		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_TIMEOUT);
+//		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_PARAM);
+//		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_BUSY);
+//		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_NOT_SUPPORTED);
+//		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_SYNC);
+//		__HAL_DMA_CLEAR_FLAG(DMA1, HAL_DMA_ERROR_REQGEN);
+//
+////	  __HAL_SPI_CLEAR_OVRFLAG(&hspi1) ;
+////     __HAL_SPI_CLEAR_CRCERRFLAG(&hspi1);
+////      __HAL_SPI_CLEAR_MODFFLAG(&hspi1);
+////       __HAL_SPI_CLEAR_FREFLAG(&hspi1);
+//
+//
+//	 }
 		  
 }
 /********************************************************************************
